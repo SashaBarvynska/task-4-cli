1cd6e121e821148aa121a1a89a8f4f273d1e1d91
Sasha Barvynska <alexbarvynska@gmail.com>
2022-09-30 15:29:27 +0300

    added pytests for task_3

--- task_3.py
+++ task_3.py
@@ -3,12 +3,16 @@ from functools import cache
 
 
 def avoid_mistake(string) -> str or tuple:
-    return get_unique_values_amount_(
+    if not isinstance(string, str) and not isinstance(string, list):
+        raise TypeError("Argument must be a string or list")
+    return get_unique_values_amount(
         tuple(string) if isinstance(string, list) else string
     )
 
 
 @cache
-def get_unique_values_amount_(string) -> int:
+def get_unique_values_amount(string) -> int:
+    if not isinstance(string, str) and not isinstance(string, tuple):
+        raise TypeError("Argument must be a string or tuple")
     string = Counter(string)
-    return len(list(filter(lambda key: string[key] == 1, string.keys())))
+    return len(list(filter(lambda key: string[key] == 1, string)))
--- test_task_3.py
+++ test_task_3.py
@@ -3,9 +3,24 @@ import pytest
 import task_3
 
 
-def test_compare():
-    assert task_3.avoid_mistake("11223") == 1
-    assert task_3.get_unique_values_amount_("1235") == 4
+def test_compare_avoid_mistake():
+    cases = [("aab", 1), (["abc1", "aaabbcc11"], 2), ("abbbccdf", 3)]
+    for text, reversed_tsxt in cases:
+        assert task_3.avoid_mistake(text) == reversed_tsxt
+
+
+def test_compare_get_unique_values_amount():
+    cases = [("aab", 1), ("abbbccdf", 3), (("apple", "banana", "cherry"), 3)]
+    for text, reversed_tsxt in cases:
+        assert task_3.get_unique_values_amount(text) == reversed_tsxt
+
+
+def test_check_wrong_type_only_list_or_str():
+    list_example_for_mistake = [123, {"Saasha": "ahsaS", "python123": "nohtyp123"}]
+    for text, example in enumerate(list_example_for_mistake):
+        with pytest.raises(Exception) as error:
+            task_3.avoid_mistake(example)
+        assert str(error.value) == "Argument must be a string or list"
 
 
 if __name__ == "__main__":
