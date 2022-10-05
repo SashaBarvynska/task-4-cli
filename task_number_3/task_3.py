df99ea517b4e9a67e6f076bc4735ecc62d274674
Sasha Barvynska <alexbarvynska@gmail.com>
2022-10-05 18:02:27 +0300

    added test for cache check

--- task_number_3/task_3.py
+++ task_number_3/task_3.py
@@ -2,27 +2,35 @@ from collections import Counter
 from functools import cache
 
 
-def avoid_mistake(string: str or list) -> str or tuple:
-    if not isinstance(string, str) and not isinstance(string, list):
-        raise TypeError("Argument must be a string or list")
-    return get_unique_values_amount(
-        tuple(string) if isinstance(string, list) else string
+def avoid_mistake(string: str or list or set) -> str or tuple:
+    if (
+        not isinstance(string, str)
+        and not isinstance(string, list)
+        and not isinstance(string, set)
+        and not isinstance(string, tuple)
+    ):
+        raise TypeError("Argument must be a string, list, tuple or set")
+    get_unique_values_amount(
+        tuple(string)
+        if isinstance(string, list)
+        or isinstance(string, set)
+        or isinstance(string, tuple)
+        else string
+    )
+    return (
+        list(map(get_unique_values_amount, string))
+        if isinstance(string, list)
+        or isinstance(string, set)
+        or isinstance(string, tuple)
+        else get_unique_values_amount(string)
     )
 
 
 @cache
 def get_unique_values_amount(string: str or tuple) -> int:
+
     if not isinstance(string, str) and not isinstance(string, tuple):
-        raise TypeError("Argument must be a string or tuple")
-    all_value_as_string = "".join(string)
+        raise TypeError("Argument must be a string, tuple")
     return len(
-        list(
-            filter(
-                lambda key: result_dict[key] == 1,
-                result_dict := Counter(all_value_as_string),
-            )
-        )
+        list(filter(lambda key: result_dict[key] == 1, result_dict := Counter(string)))
     )
-
-
-print(avoid_mistake(["abc12344", "45698"]))
--- test/test_task_3.py
+++ test/test_task_3.py
@@ -4,7 +4,13 @@ from task_number_3 import avoid_mistake, get_unique_values_amount
 
 
 @pytest.mark.parametrize(
-    "test_input,expected", [("aab", 1), (["abc1", "aaabbcc11"], 2), ("abbbccdf", 3)]
+    "test_input,expected",
+    [
+        ("aab", 1),
+        (["abc1", "aaabbcc11"], [4, 0]),
+        ("abbbccdf", 3),
+        ({"abc", "drt"}, [3, 3]),
+    ],
 )
 def test_compare_avoid_mistake(test_input, expected):
     assert avoid_mistake(test_input) == expected
@@ -12,15 +18,27 @@ def test_compare_avoid_mistake(test_input, expected):
 
 @pytest.mark.parametrize(
     "test_input,expected",
-    [("aab", 1), ("abbbccdf", 3), (("apple", "banana", "cherry"), 3)],
+    [("aab", 1), ("abbbccdf", 3), (("apple"), 3)],
 )
 def test_compare_get_unique_values_amount(test_input, expected):
     assert get_unique_values_amount(test_input) == expected
 
 
-def test_check_wrong_type_only_list_or_str():
-    list_example_for_mistake = [123, {"Saasha": "ahsaS", "python123": "nohtyp123"}]
-    for example in list_example_for_mistake:
-        with pytest.raises(Exception) as error:
-            avoid_mistake(example)
-        assert str(error.value) == "Argument must be a string or list"
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        (123),
+        ({"Saasha": "ahsaS", "python123": "nohtyp123"}),
+    ],
+)
+def test_check_wrong_type_only_list_or_str(test_input):
+    with pytest.raises(Exception) as error:
+        avoid_mistake(test_input)
+    assert str(error.value) == "Argument must be a string, list, tuple or set"
+
+
+@pytest.mark.parametrize(
+    "test_input,expected", [("cache_info" in dir(get_unique_values_amount), True)]
+)
+def test_function_contains_cache(test_input, expected):
+    assert test_input == expected
