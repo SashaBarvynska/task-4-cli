00b7d067c160f9721f41ae8f8a9fbec93c602c69
Sasha Barvynska <alexbarvynska@gmail.com>
2022-10-21 13:17:23 +0300

    corrected mistakes

--- task_number_3/collect_framework.py
+++ task_number_3/collect_framework.py
@@ -2,16 +2,31 @@ import argparse
 
 from task_3 import avoid_mistake
 
-parser = argparse.ArgumentParser(description="test_parse")
-parser.add_argument("--string")
-parser.add_argument(("--file"))
-args = parser.parse_args()
 
-if args.string is None and args.file is None:
-    parser.error("at least one of --string and --file required")
-if args.file is not None:
-    f = open(f"../{args.file}", "r")
-    avoid_mistake(str(f.read()))
-    f.close()
-else:
+def add_agrum_to_cli():
+    parser = argparse.ArgumentParser(description="Get unique values amount")
+    parser.add_argument("--string")
+    parser.add_argument("--file")
+    args = parser.parse_args()
+
+    if not args.string and not args.file:
+        parser.error("at least one of --string and --file required")
+
+    if args.file:
+        return open_file(args)
+    return read_string(args)
+
+
+def open_file(args):
+    try:
+        with open(args.file, "r") as reader:
+            print(avoid_mistake(str(reader.read())))
+    except FileNotFoundError:
+        "file {} does not exist".format(args.file)
+
+
+def read_string(args):
     avoid_mistake(args.string)
+
+
+add_agrum_to_cli()
