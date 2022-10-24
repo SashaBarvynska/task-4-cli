8d433cf00f4522ef8a7995ce1f2b216882650652
Sasha Barvynska <alexbarvynska@gmail.com>
2022-10-24 19:38:14 +0300

    added type annotations

--- task_number_3/collect_framework.py
+++ task_number_3/collect_framework.py
@@ -1,32 +1,42 @@
-import argparse
+from argparse import ArgumentParser, Namespace
+from io import TextIOWrapper
 
 from task_3 import avoid_mistake
 
 
-def add_agrum_to_cli():
-    parser = argparse.ArgumentParser(description="Get unique values amount")
+def add_agrs_to_cli() -> str:
+    parser = ArgumentParser(description="Get unique values amount")
     parser.add_argument("--string")
     parser.add_argument("--file")
     args = parser.parse_args()
 
-    if not args.string and not args.file:
-        parser.error("at least one of --string and --file required")
-
     if args.file:
-        return open_file(args)
-    return read_string(args)
+        return open_file(args.file)
+    elif args.string:
+        return read_string(args.string)
+    else:
+        return check_cli_without_args(args)
 
 
-def open_file(args):
+def check_cli_without_args(args: Namespace) -> None:
+    if not args.string and not args.file:
+        raise Exception("at least one of --string and --file required")
+
+
+def open_file(file_path: str) -> str:
     try:
-        with open(args.file, "r") as reader:
-            print(avoid_mistake(str(reader.read())))
+        with open(file_path, "r") as reader:
+            return read_file(reader)
     except FileNotFoundError:
-        "file {} does not exist".format(args.file)
+        raise FileNotFoundError(f"file {file_path} does not exist")
 
 
-def read_string(args):
-    avoid_mistake(args.string)
+def read_file(reader: TextIOWrapper) -> str:
+    return avoid_mistake(str(reader.read()))
 
 
-add_agrum_to_cli()
+def read_string(string: str) -> str:
+    avoid_mistake(string)
+
+if __name__ == "__main__":
+    add_agrs_to_cli()
