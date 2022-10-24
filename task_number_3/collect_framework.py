019afb2feffa3b992d4091673a41ef6f35d2ca3f
Sasha Barvynska <alexbarvynska@gmail.com>
2022-10-24 23:09:38 +0300

    removed extra functions

--- task_number_3/collect_framework.py
+++ task_number_3/collect_framework.py
@@ -1,42 +1,31 @@
-from argparse import ArgumentParser, Namespace
-from io import TextIOWrapper
+from argparse import ArgumentParser
 
 from task_3 import avoid_mistake
 
 
-def add_agrs_to_cli() -> str:
+def init_parser() -> str:
     parser = ArgumentParser(description="Get unique values amount")
     parser.add_argument("--string")
     parser.add_argument("--file")
     args = parser.parse_args()
-
-    if args.file:
-        return open_file(args.file)
-    elif args.string:
-        return read_string(args.string)
-    else:
-        return check_cli_without_args(args)
+    return main(args)
 
 
-def check_cli_without_args(args: Namespace) -> None:
+def main(args):
     if not args.string and not args.file:
         raise Exception("at least one of --string and --file required")
+    if args.file:
+        return avoid_mistake(str(open_file(args.file).read()))
+    elif args.string:
+        return avoid_mistake(args.string)
 
 
 def open_file(file_path: str) -> str:
     try:
-        with open(file_path, "r") as reader:
-            return read_file(reader)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"file {file_path} does not exist")
+        return open(file_path, "r")
+    except:
+        raise FileNotFoundError(f"File {file_path} does not exist")
 
-
-def read_file(reader: TextIOWrapper) -> str:
-    return avoid_mistake(str(reader.read()))
-
-
-def read_string(string: str) -> str:
-    avoid_mistake(string)
 
 if __name__ == "__main__":
-    add_agrs_to_cli()
+    init_parser()
