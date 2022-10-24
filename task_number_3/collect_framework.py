ddfbfa2efbb709cbdb72791fb4a79b8e30409293
Sasha Barvynska <alexbarvynska@gmail.com>
2022-10-24 23:46:52 +0300

    used with instead try/except

--- task_number_3/collect_framework.py
+++ task_number_3/collect_framework.py
@@ -1,31 +1,33 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, Namespace
 
 from task_3 import avoid_mistake
 
 
-def init_parser() -> str:
+def init_parser() -> Namespace:
     parser = ArgumentParser(description="Get unique values amount")
     parser.add_argument("--string")
     parser.add_argument("--file")
     args = parser.parse_args()
-    return main(args)
+    return args
 
 
-def main(args):
+def main():
+    args = init_parser()
     if not args.string and not args.file:
         raise Exception("at least one of --string and --file required")
     if args.file:
-        return avoid_mistake(str(open_file(args.file).read()))
-    elif args.string:
+        return avoid_mistake(str(open_file(args.file)))
+    if args.string:
         return avoid_mistake(args.string)
 
 
 def open_file(file_path: str) -> str:
     try:
-        return open(file_path, "r")
-    except:
+        with open(file_path, "r") as reader:
+            return reader.read()
+    except FileNotFoundError:
         raise FileNotFoundError(f"File {file_path} does not exist")
 
 
 if __name__ == "__main__":
-    init_parser()
+    main()
