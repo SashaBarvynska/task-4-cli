5281c4506bacbf2a8e61e6be76c7cdb491e3f30f
Sasha Barvynska <alexbarvynska@gmail.com>
2022-11-02 20:38:42 +0200

    created package

--- packaging_tutorial/test/test_collect_framework.py
+++ packaging_tutorial/test/test_collect_framework.py
@@ -5,6 +5,8 @@ import pytest
 
 from task_number_4 import main, open_file
 
+file_example = "demo.txt"
+
 
 @patch(
     "task_number_4.collect_framework.init_parser",
@@ -19,13 +21,13 @@ def test_main_without_args(mock_init_parser):
 
 @patch(
     "task_number_4.collect_framework.init_parser",
-    return_value=Namespace(string=None, file="demo.txt"),
+    return_value=Namespace(string=None, file=file_example),
 )
 @patch("task_number_4.collect_framework.avoid_mistake", return_value=3)
 @patch("task_number_4.collect_framework.open_file", return_value="demo string")
 def test_main_with_file(mock_open_file, mock_avoid_mistake, mock_init_parser):
     assert main() == 3
-    mock_open_file.assert_called_with("demo.txt")
+    mock_open_file.assert_called_with(file_example)
     mock_avoid_mistake.assert_called_with("demo string")
     mock_init_parser.asssert_called_once()
 
@@ -44,12 +46,12 @@ def test_main_with_string(mock_avoid_mistake, mock_init_parser):
 @patch("builtins.open", side_effect=FileNotFoundError)
 def test_open_file_error(mock_open):
     with pytest.raises(FileNotFoundError) as error:
-        open_file("demo.txt")
+        open_file(file_example)
     assert str(error.value) == "File demo.txt does not exist"
-    mock_open.assert_called_with("demo.txt", "r")
+    mock_open.assert_called_with(file_example, "r")
 
 
 @patch("builtins.open", new_callable=mock_open, read_data="Sasssssssssssss34")
 def test_open_file_success(mock_open):
-    assert open_file("demo.txt") == "Sasssssssssssss34"
-    mock_open.assert_called_with("demo.txt", "r")
+    assert open_file(file_example) == "Sasssssssssssss34"
+    mock_open.assert_called_with(file_example, "r")
